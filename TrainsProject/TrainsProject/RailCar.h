c3a5eb710cbb1cda3392b32ab8bfd0580a3b46ce
Achka <andrianasolyna@gmail.com>
2016-09-21 19:20:53 +0300

    RailCar and Train classes

---
+++ TrainsProject/TrainsProject/RailCar.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <vector>
+using namespace std;
+enum class TypeOfRailCar
+{
+	FirstClass,// люкс
+	SecondClass, // купе
+	ThirdClass, // плацкарт
+	DiningCar, // вагон-ресторан
+};
+class RailCar
+{
+	unsigned number;
+	unsigned numberOfSeats;
+	TypeOfRailCar type;
+	vector<unsigned> vectorOfBookedSeats; // push to this vector numberOfseat, that is ordered
+public:
+	RailCar();
+	RailCar(unsigned, unsigned, TypeOfRailCar, vector<unsigned>);
+	void setNumber(unsigned);
+	void setNumberOfSeats(unsigned);
+	void pushSeatToVectorOfBookedSeats(unsigned);
+	void setType(TypeOfRailCar);
+	unsigned getNumber() const;
+	unsigned getNumberOfSeats() const;
+	TypeOfRailCar getType() const;
+	vector<unsigned> getVectotOfBookedSeats() const;
+	void draw();
+};
+
