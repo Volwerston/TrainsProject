9007b33a96fe4b07cc4a65fd445d5307e2e125d8
Ika <ikatsadze97@outlook.com>
2016-09-21 20:23:13 +0300

    Add tinyxml2

--- TrainsProject/TrainsProject/main.cpp
+++ TrainsProject/TrainsProject/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
+#include "tinyxml2\tinyxml2.h"
 
 using namespace std;
+using namespace tinyxml2;
 
 int main()
 {
+	
+
+
 	cout << "Hello world" << endl;
 	system("pause");
 	return 0;
